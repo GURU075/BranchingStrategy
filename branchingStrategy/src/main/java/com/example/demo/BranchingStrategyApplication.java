6bb25552fc52dfe7588afaaf61452e3bad4d6cb6
GURU <GURU@DESKTOP-475807N>
2023-09-24 21:56:57 +0530

    intial push

---
+++ branchingStrategy/src/main/java/com/example/demo/BranchingStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchingStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchingStrategyApplication.class, args);
+	}
+
+}
